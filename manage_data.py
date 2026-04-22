86890c55d54728db1d3aa075ae3f9771f53d5789
Mark London <mark.london@ons.gov.uk>
2026-04-22 10:41:30 +0100

    started first file as comments and plan

---
+++ manage_data.py
@@ -0,0 +1,5 @@
+# we shall read in passengar airport data
+
+# we are testing the hypothesis that the start of the Ukrain war
+# has had an impact on the number of passengers flying in and out of airports in the world
+# Specifically to or from the UK and Eastern Europe
