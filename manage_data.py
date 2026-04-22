52224f6a663e5ff3e511e44472dfe86f009ac4c2
Beardsmore <liam.beardsmore@ons.gov.uk>
2026-04-22 11:00:26 +0100

    updated code

--- manage_data.py
+++ manage_data.py
@@ -3,3 +3,8 @@
 # we are testing the hypothesis that the start of the Ukrain war
 # has had an impact on the number of passengers flying in and out of airports in the world
 # Specifically to or from the UK and Eastern Europe
+
+# read data
+import csv
+import pandas
+table1 = pandas.read_csv('D:\repos\hackathon\table1.csv')
